feb167f6330467eeb01a9c66fce49404280b2014
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-06-19 10:20:36 -0500

    Search by name and region (not optimized)

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -12,9 +12,19 @@ export class CountriesService {
 
   constructor(private http: HttpClient) {}
 
-  searchByCapital(term: string): Observable<Country[]> {
+  searchByCapital(capital: string): Observable<Country[]> {
     return this.http
-      .get<Country[]>(`${this.apiUrl}/capital/${term}`)
+      .get<Country[]>(`${this.apiUrl}/capital/${capital}`)
+      .pipe(catchError((error) => of([])));
+  }
+  searchByName(name: string): Observable<Country[]> {
+    return this.http
+      .get<Country[]>(`${this.apiUrl}/name/${name}`)
+      .pipe(catchError((error) => of([])));
+  }
+  searchByRegion(region: string): Observable<Country[]> {
+    return this.http
+      .get<Country[]>(`${this.apiUrl}/region/${region}`)
       .pipe(catchError((error) => of([])));
   }
 }
