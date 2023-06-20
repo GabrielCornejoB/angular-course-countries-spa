61632952968b06d8cc7612cab04fb8573717de67
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-06-19 20:09:43 -0500

    Debounce subscription cleanup

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,25 +1,47 @@
 import {
   Component,
-  ElementRef,
   EventEmitter,
   Input,
   Output,
-  ViewChild,
+  OnInit,
+  OnDestroy,
 } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: [],
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
+
   @Input()
   public placeholder: string = '';
 
   @Output()
   public onValue = new EventEmitter<string>();
 
-  public emitInputValue = (value: string): void => {
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncerSubscription = this.debouncer
+      .pipe(debounceTime(350))
+      .subscribe((value) => {
+        this.onDebounce.emit(value);
+      });
+  }
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
+  }
+
+  public emitInputValue(value: string): void {
     this.onValue.emit(value);
-  };
+  }
+
+  public onKeyPress(searchTerm: string): void {
+    this.debouncer.next(searchTerm);
+  }
 }
